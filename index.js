789f71b2c8306af0b435c801f4c31b5556634a22
hardx4 <felipe_f0@hotmail.com>
2022-02-07 19:36:01 -0400

    Update index.js
    If the configuration file does not contain basic configuration data for logs, it temporarily creates in memory the object loaded from the config.json file

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ var configFile = 'config.json';
 
 // Read configuration file data
 try {
-    global.config = JSON.parse(fs.readFileSync(configFile));    
+    global.config = JSON.parse(fs.readFileSync(configFile)); 
+    if(typeof config.logging == "undefined"){
+        config.logging = {
+            dirname:"logs",
+            flushInterval:5,
+            files:{level:"info"},
+            console:{level:"info",colors: true}
+        };        
+    }
 }
 catch(e){
     console.error('Failed to read config file ' + configFile + '\n\n' + e);
